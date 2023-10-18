fafe7e1620fc7d0c13d79df69fa0e861315d4e03
jack12347410 <jack12347410@gmail.com>
2023-10-18 10:42:19 +0800

    redistest

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Redis Test");
+
+            var redis = new RedisService();
+            // 建立 Redis 連線
+            var redisConfiguration = new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }, // Redis 伺服器位置和端口號
+                AbortOnConnectFail = false // 在連接失敗時不中止重試
+            };
+            var db = redis.Connect(redisConfiguration.ToString());
+            Console.WriteLine($"redis is connect {redis.IsConnected(redisConfiguration.ToString())}");
+
+            // 寫入鍵值對
+            string key = "example_key";
+            string value = "Hello, Redis! ";
+            var model = new Model
+            {
+                Name = "小名",
+                Age = 30
+            };
+            db.Set(key, model);
+
+            // 讀取鍵值對
+            var retrievedValue = db.Get<Model>(key);
+            Console.WriteLine($"Key: {key}, Value: {retrievedValue.Name + retrievedValue.Age}");
+
+
+            //var mux = ConnectionMultiplexer.Connect("localhost:6379, abortConnect=false");
+            //var db = mux.GetDatabase();
+            //Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: Redis connection is established.");
+
+            //var key = "MyCache:foo";
+            //var value = "shared cache data";
+
+            //db.StringSet(key, value);
+            //Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}: StringSet: key:{key}, value:{value}");
+            //Console.WriteLine("Press any key to continue...");
+
+            //Console.WriteLine($"get MyCache:foo={db.StringGet(key)}"); 
+            //Console.Read();
+        }
+
+        public class Model
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ RedisService.cs
@@ -0,0 +1,72 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisTest
+{
+    public sealed class RedisService
+    {
+        private static ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> _connectionPool = new();
+        public IDatabase Connect(string setting = "localhost")
+        {
+            var connMultiplexer = _connectionPool.GetOrAdd(setting,
+                new Lazy<ConnectionMultiplexer>(() =>
+                {
+                    return ConnectionMultiplexer.Connect(setting);
+                }));
+
+            return connMultiplexer.Value.GetDatabase();
+        }
+
+        public IDatabase GetDatabase(string setting = "localhost")
+        {
+            if (_connectionPool.TryGetValue(setting, out var connectionMultiplexer))
+            {
+                return connectionMultiplexer.Value.GetDatabase();
+            }
+
+            return default;
+        }
+
+        public bool IsConnected(string setting = "localhost")
+        {
+            if (_connectionPool.TryGetValue(setting, out var connectionMultiplexer))
+            {
+                return connectionMultiplexer.Value.IsConnected;
+            }
+
+            return false;
+        }
+
+        //private static string _settingOption;
+        //private static Lazy<RedisService> _lazy = new Lazy<RedisService>(() =>
+        //{
+        //    if (String.IsNullOrEmpty(_settingOption)) throw new InvalidOperationException("Please call Init() first.");
+        //    return new RedisService();
+        //});
+        //private ConnectionMultiplexer ConnectionMultiplexer;
+        //public IDatabase Database => this.ConnectionMultiplexer.GetDatabase();
+
+        //public static RedisService Instance
+        //{
+        //    get
+        //    {
+        //        return _lazy.Value;
+        //    }
+        //}
+
+        //private RedisService()
+        //{
+        //    ConnectionMultiplexer = ConnectionMultiplexer.Connect(_settingOption);
+        //}
+
+        //public static void Init(string settingOption)
+        //{
+        //    _settingOption = settingOption;
+        //}
+    }
+}
